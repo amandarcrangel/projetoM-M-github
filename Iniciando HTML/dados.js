8823b033298c3bb2bcb925c23c987eb8a83732f3
Amanda Rangel <amandarangel897@gmail.com>
2024-09-09 14:39:07 -0300

    Add files via upload

---
+++ Iniciando HTML/dados.js
@@ -0,0 +1,22 @@
+let dados = [
+  {
+      titulo: "Manutenção preventiva",
+      descricao: "Oferecemos manutenção preventiva para evitar falhas e garantir o desempenho contínuo dos equipamentos.",
+      link: ""
+  },
+  {
+      titulo: "Manutenção Preditiva",
+      descricao: "Realizamos manutenção preditiva para monitorar e analisar o desempenho dos equipamentos, antecipando falhas e garantindo operações contínuas e eficientes.",
+      link: ""
+  },
+  {
+      titulo: "Manutenção corretiva",
+      descricao: "Realizamos manutenção corretiva para resolver problemas e restaurar o funcionamento ideal dos equipamentos.",
+      link: ""
+  },
+  {
+      titulo: "Instalação de sistemas",
+      descricao: "Nossa equipe especializada oferece um serviço completo de instalação de diversos sistemas, Ar-condicionado; Iluminação; instalação de elétrica entreoutros.",
+      link: ""
+  }
+];
